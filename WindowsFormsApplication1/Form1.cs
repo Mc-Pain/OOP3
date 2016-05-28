97e32fc5dc3527b64715a2ed52561650b3c2911f
Mc-Pain <McPain2@mail.ru>
2016-05-29 01:54:49 +0300

    v0.2
    Timer added, all classes finished

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -19,17 +19,41 @@ namespace WindowsFormsApplication1
         }
 
         volatile bool working = false;
+        static DateTime globaltime = new DateTime(2016, 05, 01);
+
+        private void Init()
+        {
+            int num = 6;
+            CAirport[] airports = new CAirport[num];
+            CPlane[] planes = new CPlane[num];
+            for (int i = 0; i < num; i++)
+            { //создаём аэропорты и самолёты
+                airports[i] = new CAirport(i.ToString());
+                planes[i] = new CPlane(airports[i]);
+
+                Invoke(new OutputDelegate(Output), new object[] { String.Format("Работа... {0}", globaltime.ToString("f")) });
+            }
+        }
+
+        private static void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+        { 
+            globaltime = globaltime.AddMinutes(10);
+        }
+
+        private void Timing()
+        {
+            System.Timers.Timer timer = new System.Timers.Timer(500);
+            timer.Elapsed += OnTimedEvent;
+            timer.Start();
+        }
 
         private void Modelling()
         { //здесь будет моделирование
-            int i = 0;
             while (working)
             {
-                if (i > 1000)
-                    i = 0;
-                Invoke(new OutputDelegate(Output), new object[] { String.Format("Работа... {0}", i++) });
+                Invoke(new OutputDelegate(Output), new object[] { String.Format("Работа... {0}", globaltime.ToString("f")) });
             }
-            Invoke(new OutputDelegate(Output), new object[] { String.Format("Останов: {0}", i) });
+            Invoke(new OutputDelegate(Output), new object[] { String.Format("Останов {0}", globaltime.ToString("f")) });
         }
 
         private void Output(string msg)
@@ -40,18 +64,22 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Thread thr = new Thread(new ThreadStart(Modelling));
-            thr.IsBackground = true;
-            thr.Start();
+            Thread thr_model = new Thread(new ThreadStart(Modelling));
+            thr_model.IsBackground = true;
+            thr_model.Start();
+            Thread thr_time = new Thread(new ThreadStart(Timing));
+            thr_time.IsBackground = true;
+            thr_time.Start();
+            working = !working;
             if (working)
             {
-                button1.Text = "Запуск";
+                Init();
+                button1.Text = "Остановить";
             }
             else
             {
-                button1.Text = "Остановить";
+                button1.Text = "Запуск";
             }
-            working = !working;
         }
     }
 
@@ -73,8 +101,8 @@ namespace WindowsFormsApplication1
         private CAirport landed_in;
         private int num_engaged; //занято мест
 
-        private void CPlane(CAirport airport)
-        {
+        public CPlane(CAirport airport)
+        { //создание самолёта
             landed = true;
             landed_in = airport;
             num_engaged = 0;
@@ -83,60 +111,65 @@ namespace WindowsFormsApplication1
         }
 
         private void Land(CAirport airport)
-        {
+        { //приземление
             if (!landed)
-            {
+            { //самолёт в воздухе
                 landed = true;
                 landed_in = airport;
+                for (int i = 0; i < 100; i++)
+                {
+                    this.kick_passenger(i); //высаживаем i-го пассажира
+                }
+                airport.busy = false;
+                flight = null; //задание выполнено
             }
             else
-            {
+            { //самолёт не в воздухе
                 _Exception exc = new _Exception("Только Чаке может сажать самолёт два раза подряд");
                 throw (exc);
             }
         }
 
         private void Takeoff()
-        {
+        { //взлёт
             if (landed)
-            {
-
+            { //самолёт не в воздухе
                 landed_in = null;
                 landed = false;
             }
             else
-            {
+            { //самолёт в воздухе
                 _Exception exc = new _Exception("Самолет уже летит, а ты его запустить хочешь");
                 throw (exc);
             }
         }
 
         private void take_passenger(CPassenger passenger, CTicket ticket)
-        {
+        { //приём пассажира на борт
             if (landed)
-            {
+            { //самолёт не в воздухе
                 if (passengers[ticket.GetSeat()] == null)
-                {
+                { //место свободно
                     passengers[ticket.GetSeat()] = passenger;
                     num_engaged++;
                 }
                 else
-                {
+                { //место занято
                     _Exception exc = new _Exception("Вы сели на моё место!");
                     throw (exc);
                 }
             }
             else
-            {
+            { //самолёт в воздухе
                 _Exception exc = new _Exception("Супермена чтоли собрался сажать?");
                 throw (exc);
             }
         }
 
         private void kick_passenger(int seat)
-        {
+        { //высадка пассажиров
             if (landed)
-            {
+            { //самолёт не в воздухе
                 if (passengers[seat] != null)
                 {
                     passengers[seat] = null;
@@ -144,21 +177,21 @@ namespace WindowsFormsApplication1
                 }
             }
             else
-            {
+            { //самолёт в воздухе
                 _Exception exc = new _Exception("THIS IS SPARTA!!!1");
                 throw (exc);
             }
         }
 
         private void setflight(CFlight new_flight)
-        {
+        { //установка нового рейса
             if (flight != null)
-            {
+            { //рейс уже установлен
                 _Exception exc = new _Exception("Менять рейс во время полёта - не труЪ");
                 throw (exc);
             }
             else
-            {
+            { //рейс не установлен
                 flight = new_flight;
             }
         }
@@ -173,19 +206,68 @@ namespace WindowsFormsApplication1
         private DateTime landing; //время посадки
         private CPlane plane; //самолет
         private CTicket[] tickets; //билеты на рейс
+
+        private CFlight(string id, CAirport new_start, CAirport new_end, DateTime new_takeoff, DateTime new_landing)
+        { //создание рейса
+            name = id;
+            start = new_start;
+            end = new_end;
+            takeoff = new_takeoff;
+            landing = new_landing;
+            tickets = new CTicket[100];
+
+            Random rand = new Random();
+            for (int i = 0; i < 100; i++)
+            { //генерация билетов и пассажиров
+                if (rand.Next(100) < 90)
+                { //вероятность покупки билета - 90%
+                    int time = 10 + rand.Next(170);
+                    CPassenger pass = new CPassenger(takeoff);
+                    tickets[i] = new CTicket(pass, this, i);
+                }
+            }
+        }
+
+        private void Delay(int min)
+        { //задержать рейс
+            takeoff = takeoff.AddMinutes(min);
+            landing = landing.AddMinutes(min);
+        }
     }
 
     public class CPassenger
     {
         private DateTime arrival; //прибытие в аэропорт
         private CTicket ticket; //билет
+
+        public CPassenger(DateTime new_arrival)
+        { //создание пассажира
+            new_arrival = arrival;
+        }
+
+        public void SetTicket(CTicket new_ticket)
+        {
+            if (ticket == null)
+            {
+                new_ticket = ticket;
+            }
+            else
+            {
+                _Exception exc = new _Exception("Зачем чуваку два билета?");
+                throw (exc);
+            }
+        }
     }
 
     public class CAirport
     {
         private string name;
-        private Queue<CPlane> planequeue;
-        private List<CPassenger> passengerlist;
+        public bool busy = false;
+
+        public CAirport(string new_name)
+        {
+            name = new_name;
+        }
     }
 
     public class CTicket
@@ -193,6 +275,13 @@ namespace WindowsFormsApplication1
         private CPassenger passenger;
         private CFlight flight;
         private int seat;
+
+        public CTicket(CPassenger new_passenger, CFlight new_flight, int number)
+        {
+            passenger = new_passenger;
+            flight = new_flight;
+            seat = number;
+        }
 
         public int GetSeat()
         {
